6e8a0281e6cad12b4664af79cb72f96926e4cb82
ericnivek <kevin.angle001@gmail.com>
2025-08-26 20:04:42 -0400

    Added circleshape.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -12,6 +14,9 @@ def main():
                 return
         screen.fill("Black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
         main()
